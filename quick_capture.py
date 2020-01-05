6b2d232afe41553bdbea38c1acb0b74b8850b2e7
Nate <nmuth87@gmail.com>
2020-01-05 11:05:33 -0600

    added settings

--- quick_capture.py
+++ quick_capture.py
@@ -4,6 +4,9 @@ import sys
 picture_name = sys.argv[1]
 
 camera = PiCamera()
-camera.capture(f'aero_garden_5min/{picture_name}.jpg')
 
-# print(f'picture captured and saved as {picture_name}.jpg')
+# change camera settings
+camera.iso = 800
+camera.exposure_mode = 'sports'
+
+camera.capture(f'aero_garden_1hour/{picture_name}.jpg')
