94390fa614b6c56ea7ef7df258cf9df84f54dfa3
nmuth87@gmail.com <nmuth87@gmail.com>
2020-01-05 11:06:00 -0600

    small

--- quick_capture.py
+++ quick_capture.py
@@ -4,6 +4,6 @@ import sys
 picture_name = sys.argv[1]
 
 camera = PiCamera()
-camera.capture(f'aero_garden_5min/{picture_name}.jpg')
+camera.capture(f'/home/pi/Projects/pi-timelapse/aero_garden_5min/{picture_name}.jpg')
 
 # print(f'picture captured and saved as {picture_name}.jpg')
