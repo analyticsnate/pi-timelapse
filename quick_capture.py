e1b1e3b600c36ba7418cd78337b2cd11aec13fdd
nmuth87 <nmuth87@gmail.com>
2020-10-15 15:06:19 -0500

    added aws upload

--- quick_capture.py
+++ quick_capture.py
@@ -1,4 +1,5 @@
 from picamera import PiCamera
+from nolesmuth_aws_s3 import upload_file
 import sys
 
 picture_name = sys.argv[1]
@@ -9,4 +10,6 @@ camera = PiCamera()
 camera.iso = 800
 camera.exposure_mode = 'sports'
 
-camera.capture(f'aero_garden_1hour/{picture_name}.jpg')
+camera.capture(f'{picture_name}.jpg')
+
+upload_file(f'{picture_name}.jpg')
